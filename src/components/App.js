8ac3fbe733c14b42b278862483cd6177905d31c0
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-24 11:12:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 
+import React from "react";
+import Tabs from "./Tabs";
+
+const App=()=>{
+    return(
+        <div>
+           {/*  <div className="tabs">
+                <div className="tab" id="Home-Tab">Home</div>
+                <div className="tab" id="About-Tab">About</div>
+                <div className="tab" id="Features-Tab">Features</div>
+            </div> */}
+            <Tabs />
+            <div className="viewport">Pages Go Here</div>
+        </div>
+    )
+}
+
+export default App
